7a2a1f7c9cc2c4705cc6085538e0e1c847078c61
Dan Cryer <dan@dancryer.com>
2019-03-18 13:11:36 +0000

    Add Java version

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,8 @@
 echo -e "\033[34mPreparing...\033[0m"
 gcc mandlebrot.c -o mandlebrot-c -O3 -mavx2 -fomit-frame-pointer -funroll-loops -ftree-vectorize -march=native 2>&1
 go build -o mandlebrot-go mandlebrot.go
+mv mandlebrot.java Mandlebrot.java
+javac Mandlebrot.java
 echo ""
 
 echo -e "\033[31mC version\033[0m"
@@ -13,8 +15,12 @@ echo -e "\033[31mGo version\033[0m"
 time ./mandlebrot-go
 echo ""
 
-echo -e "\033[31mNode version\033[0m"
+echo -e "\033[31mNodeJS version\033[0m"
 time node mandlebrot.js
+echo ""
+
+echo -e "\033[31mJava version\033[0m"
+time java Mandlebrot
 echo ""
 
 echo -e "\033[31mPHP version\033[0m"
@@ -24,5 +30,7 @@ echo ""
 echo -e "\033[34mCleaning up...\033[0m"
 rm -f mandlebrot-c
 rm -f mandlebrot-go
+rm -f Mandlebrot.class
+mv Mandlebrot.java mandlebrot.java
 echo "Done."
 echo ""
